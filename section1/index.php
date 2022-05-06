045bb319f387058f7b9c2056976d42b084c5b5a9
taka01150810 <taka01150810@gmail.com>
2022-05-06 13:13:44 +0900

    PHPの基礎(PHPの書き方)

---
+++ section1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo ('test');//結果 test
+/*
+数字は半角
+123 → 数字として認識
+１２３ → 文字として認識
+*/
+echo('こんにちは');//結果 こんにちは
+echo("こんにちは");//結果 こんにちは
+echo('こん"にちは');//結果 こん"にちは
+
+?>
+
+</body>
+</html>
