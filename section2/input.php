cf84913cbb883c45a9e57000234b152f95fc4567
taka01150810 <taka01150810@gmail.com>
2022-05-10 09:54:42 +0900

    お問い合わせフォーム(フォームセキュリティ XSS.クリックジャッキング対策)

--- section2/input.php
+++ section2/input.php
@@ -1,4 +1,8 @@
 <?php
+
+//クリックジャッキング対策
+header('X-FRAME-OPTION:DENY');
+
 $pageFlag = 0;
 if(!empty($_POST['btn_confirm'])){
     $pageFlag = 1;
@@ -6,6 +10,11 @@ if(!empty($_POST['btn_confirm'])){
 
 if(!empty($_POST['btn_submit'])){
     $pageFlag = 2;
+}
+
+//XSS対策
+function h($str){
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 ?>
@@ -21,9 +30,9 @@ if(!empty($_POST['btn_submit'])){
     <?php if($pageFlag === 0): ?>
     <form method="POST" action="input.php">
         氏名
-        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){ echo($_POST['your_name']); } ?>"><br>
+        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){ echo h(($_POST['your_name'])); } ?>"><br>
         メールアドレス
-        <input type="email" name="email" value="<?php if(!empty($_POST['your_name'])){ echo $_POST['email']; } ?>">
+        <input type="email" name="email" value="<?php if(!empty($_POST['your_name'])){ echo h($_POST['email']); } ?>">
         <input type="submit" name="btn_confirm" value="確認する">
     </form>
     <?php endif; ?>
@@ -31,13 +40,13 @@ if(!empty($_POST['btn_submit'])){
     <?php if($pageFlag === 1): ?>
     <form method="POST" action="input.php">
         氏名
-        <?php echo $_POST['your_name']; ?><br>
+        <?php echo H($_POST['your_name']); ?><br>
         メールアドレス
-        <?php echo $_POST['email']; ?><br>
+        <?php echo h($_POST['email']); ?><br>
         <input type="submit" name="back" value="戻る">
         <input type="submit" name="btn_submit" value="送信する">
-        <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+        <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+        <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
     </form>
     <?php endif; ?>
 
