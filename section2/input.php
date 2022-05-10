c5f45dd9f658ce9ba7fb00c524f2c52b5e8fa669
taka01150810 <taka01150810@gmail.com>
2022-05-10 10:10:06 +0900

    お問い合わせフォーム(フォームセキュリティ CSRF)

--- section2/input.php
+++ section2/input.php
@@ -1,5 +1,12 @@
 <?php
 
+//CSRF 偽物のinput.php -> 悪意のあるページ
+/*
+$_SESSIONを使ったトークンを発行する
+$_GETや$_POSTは一回きりだが$_SESSIONは残る
+*/
+session_start();
+
 //クリックジャッキング対策
 header('X-FRAME-OPTION:DENY');
 
@@ -28,16 +35,27 @@ function h($str){
 </head>
 <body>
     <?php if($pageFlag === 0): ?>
+        <?php
+        if(!isset($_SESSION['csrfToken'])){
+            $csrfToken = bin2hex(random_bytes(32));
+            //bin2hex ... 16進数に変換(バイナリになってしまうので)
+            //random_bytes ... 暗号を作る
+            $_SESSION['csrfToken'] = $csrfToken;
+        }
+        $token = $_SESSION['csrfToken'];
+        ?>
     <form method="POST" action="input.php">
         氏名
         <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){ echo h(($_POST['your_name'])); } ?>"><br>
         メールアドレス
         <input type="email" name="email" value="<?php if(!empty($_POST['your_name'])){ echo h($_POST['email']); } ?>">
         <input type="submit" name="btn_confirm" value="確認する">
+        <input type="hidden" name="csrf" value="<?php echo $token; ?>">
     </form>
     <?php endif; ?>
 
     <?php if($pageFlag === 1): ?>
+    <?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
     <form method="POST" action="input.php">
         氏名
         <?php echo H($_POST['your_name']); ?><br>
@@ -47,11 +65,17 @@ function h($str){
         <input type="submit" name="btn_submit" value="送信する">
         <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
         <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+        <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
     </form>
+    <?php endif; ?>
     <?php endif; ?>
 
     <?php if($pageFlag === 2): ?>
+        <?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
         送信が完了しました
+        <?php unset($_SESSION['csrfToken']); ?>
+        <!-- 完了画面が表示されるタイミングでCSRFのTokenを外れる -->
+        <?php endif; ?>
     <?php endif; ?>
 </body>
 </html>
