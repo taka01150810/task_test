ab5636f1d86c79b6d1348395f261efe66eb996d2
taka01150810 <taka01150810@gmail.com>
2022-05-16 13:29:34 +0900

    セッションや高度な関数(タイプヒンティング)

--- section4/function.php
+++ section4/function.php
@@ -13,4 +13,23 @@ echo '<br>';
 //引数あり
 defaultValue('テスト');
 //結果 テストです
+
+echo 'タイプヒンティングテスト'.'<br>';
+function noTypeHint($string){
+    var_dump($string);
+}
+noTypeHint(['テスト']);
+//結果 array(1) { [0]=> string(9) "テスト" }
+//引数string予定に配列→エラーは出ないので
+echo '<br>';
+
+//タイプヒンティング(引数に型を指定。型が違うとエラー)
+function typeTest(string $string){
+    var_dump($string);
+}
+typeTest('配列文字');
+//結果 string(12) "配列文字"
+
+// typeTest(['配列文字']);
+//結果 引数にstringと指定しているのに配列なのでエラー
 ?>
