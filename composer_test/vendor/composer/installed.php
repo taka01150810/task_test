e2f119c7a4b789477b12fb9882f85fbca721b938
taka01150810 <taka01150810@gmail.com>
2022-05-17 12:04:43 +0900

    オブジェクト指向やモダンPHP(composer実行)

---
+++ composer_test/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '65ac0a00f089f8cf6913027ad3a98a1d6dac91da',
+        'name' => 'miyanakayoshiaki/composer_test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'miyanakayoshiaki/composer_test' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '65ac0a00f089f8cf6913027ad3a98a1d6dac91da',
+            'dev_requirement' => false,
+        ),
+    ),
+);
