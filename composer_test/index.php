1d6f230643261750ed495517ce955d3cb988f300
taka01150810 <taka01150810@gmail.com>
2022-05-18 15:25:55 +0900

    オブジェクト指向やモダンPHP(Carbonライブラリ実 行)

--- composer_test/index.php
+++ composer_test/index.php
@@ -7,3 +7,11 @@ use App\Controllers\TestController;
 $app = new TestController;
 $app->run();
 //結果 hello world
+
+use Carbon\Carbon;
+
+echo Carbon::now();
+//結果 2022-05-18 06:23:56
+
+echo Carbon::now()->format('今日はY年m月d日だよ!');
+//結果 今日は2022年05月18日だよ!
