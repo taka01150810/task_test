9837043bb4252c1b63de7fc2ca49ab98e65b8deb
taka01150810 <taka01150810@gmail.com>
2022-05-20 11:05:23 +0900

    laravel入門(クエリビルダ)

--- section7/task_test/app/Http/Controllers/TestController.php
+++ section7/task_test/app/Http/Controllers/TestController.php
@@ -12,6 +12,61 @@ class TestController extends Controller
     {
         $values = Test::all();
 
+        /* クエリビルダ
+        Select,whereなどSQLに近い構文
+        DB::table(テーブル名)-> とつなぐ
+        */
+        $tests = DB::table('tests')->get();
+        var_dump($tests);
+        /* 結果
+        object(Illuminate\Support\Collection)#362 (1) {
+        ["items":protected]=>
+        array(2) {
+            [0]=>
+            object(stdClass)#1258 (4) {
+            ["id"]=>
+            int(1)
+            ["text"]=>
+            string(10) "テスト1"
+            ["created_at"]=>
+            NULL
+            ["updated_at"]=>
+            NULL
+            }
+            [1]=>
+            object(stdClass)#1259 (4) {
+            ["id"]=>
+            int(2)
+            ["text"]=>
+            string(10) "テスト2"
+            ["created_at"]=>
+            NULL
+            ["updated_at"]=>
+            NULL
+            }
+        }
+        }
+        */
+        $tests = DB::table('tests')->select('id')->get();
+        var_dump($tests);//dd = die + var_dump
+        /* 結果
+        object(Illuminate\Support\Collection)#379 (1) {
+        ["items":protected]=>
+        array(2) {
+            [0]=>
+            object(stdClass)#1348 (1) {
+            ["id"]=>
+            int(1)
+            }
+            [1]=>
+            object(stdClass)#1347 (1) {
+            ["id"]=>
+            int(2)
+            }
+        }
+        }
+        */
+
         /*
         データベースからデータを取得時はコレクション型になっている。
         コレクション型には専門の関数は多数あり、メソッドチェーンで記述可能。
