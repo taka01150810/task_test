b3d80e5b1341ee9c1b4e6c7f21ed06222ac5905f
taka01150810 <taka01150810@gmail.com>
2022-05-20 11:34:18 +0900

    laravel入門(ブレード)

--- section7/task_test/resources/views/tests/test.blade.php
+++ section7/task_test/resources/views/tests/test.blade.php
@@ -5,3 +5,10 @@
 {{ $value->text }}
 {{-- 結果 テスト1 --}}
 @endforeach
+{{-- 
+    {{ }} でエスケープ処理して表示
+    (自動的にhtmlspecialchairsを設定しXSS攻撃を防ぐ)
+    @csrfでCSRF対策
+    @foreach @endforeach で配列表示
+    @section @yieldなどでテンプレート読み込み 
+--}}
