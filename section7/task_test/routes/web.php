ada1bd4b83e67b4ab08764a9c4e8ac2209941467
taka01150810 <taka01150810@gmail.com>
2022-05-23 14:18:13 +0900

    簡易Webアプリ(show 表示画面)

--- section7/task_test/routes/web.php
+++ section7/task_test/routes/web.php
@@ -26,6 +26,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
     Route::get('index', 'ContactFormController@index')->name('contact.index');
     Route::get('create', 'ContactFormController@create')->name('contact.create');
     Route::post('store', 'ContactFormController@store')->name('contact.store');
+    Route::get('show/{id}', 'ContactFormController@show')->name('contact.show');
 });
 
 // Route::resource('contacts', ContactFormController::class);
