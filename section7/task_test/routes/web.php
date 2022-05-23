0a2d30d10f671890bbb2f4aef50f49b4b8743959
taka01150810 <taka01150810@gmail.com>
2022-05-23 14:38:57 +0900

    簡易Webアプリ(update 更新画面)

--- section7/task_test/routes/web.php
+++ section7/task_test/routes/web.php
@@ -28,6 +28,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
     Route::post('store', 'ContactFormController@store')->name('contact.store');
     Route::get('show/{id}', 'ContactFormController@show')->name('contact.show');
     Route::get('edit/{id}', 'ContactFormController@edit')->name('contact.edit');
+    Route::post('update/{id}', 'ContactFormController@update')->name('contact.update');
 });
 
 // Route::resource('contacts', ContactFormController::class);
